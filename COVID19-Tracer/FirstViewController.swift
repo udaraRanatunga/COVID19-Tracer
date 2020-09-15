9f817d21725749f54ca82edcf0656e758e181394
Udara Ranatunga <lily2katie@gmail.com>
2020-09-15 17:37:11 +0530

    Initial Commit

---
+++ COVID19-Tracer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  COVID19-Tracer
+//
+//  Created by Udara Ranatunga on 9/15/20.
+//  Copyright © 2020 Udara Ranatunga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COVID19-Tracer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  COVID19-Tracer
+//
+//  Created by Udara Ranatunga on 9/15/20.
+//  Copyright © 2020 Udara Ranatunga. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
