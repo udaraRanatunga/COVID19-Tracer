cef5c3865901cc6ed352a04bc25eb196c68244a5
Udara Ranatunga <lily2katie@gmail.com>
2020-09-18 07:33:55 +0530

    Added firebase, created new view controllers for the new pages

--- COVID19-Tracer/View/UpdateViewController.swift
+++ COVID19-Tracer/View/UpdateViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class UpdateViewController: UIViewController {
 
@@ -14,8 +16,11 @@ class UpdateViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .lightGray;
+        
+        
     }
 
+   
 
 }
 
